293d0b9d875d6f56356cc5a26bf1a36176da81b2
kate1301@naver.com <kate1301@naver.com>
2021-03-28 00:49:49 +0900

    연산자 오버로딩

--- cppProject1/tryfirst.cpp
+++ cppProject1/tryfirst.cpp
@@ -1361,4 +1361,73 @@ void Book::set_percent()
 	percent_ = (double)current_page_ / total_page_ * 100;
 }*/
 
-// 연산자 오버로딩
+// 연산자 오버로딩 - 1
+/*class Position
+{
+private:
+	double x_;
+	double y_;
+public:
+	Position(double x, double y);	// 생성자 
+	void Display();
+	Position operator-(const Position& other);	// - 연산자 함수
+	// 연산자 함수: 연산자를 오버로딩하기 위해서 사용하는 것
+	// 연산자 함수의 문법: operator오버로딩할연산자(매개변수목록)
+	// 즉, 뺄셈 연산자(-)를 두 Position 객체의 중간 좌표 구하는 연산으로!
+};
+int main(void)
+{
+	Position pos1 = Position(3.3, 12.5);
+	Position pos2 = Position(-14.4, 7.8);
+	Position pos3 = pos1 - pos2;
+	pos3.Display();
+	return 0;
+}
+Position::Position(double x, double y)
+{
+	x_ = x;
+	y_ = y;
+}
+Position Position::operator-(const Position& other)
+{	return Position((x_ + other.x_) / 2, (y_ + other.y_) / 2);	}
+void Position::Display()
+{
+	cout << "두 지점의 중간 지점의 좌표는 x좌표가 " << this->x_
+		<< "이고, y좌표가 " << this->y_ << "입니다." << endl;
+}*/
+
+// 연산자 오버로딩 - 2
+/*// 앞의 예제를 전역 함수로 정의한 것은 다음과 같다
+class Position
+{
+private:
+	double x_;
+	double y_;
+public:
+	Position(double x, double y); 
+	void Display();
+	friend Position operator-(const Position& pos1, const Position& pos2);
+};
+int main(void)
+{
+	Position pos1 = Position(3.3, 12.5);
+	Position pos2 = Position(-14.4, 7.8);
+	Position pos3 = pos1 - pos2;
+	pos3.Display();
+	return 0;
+}
+Position::Position(double x, double y)
+{
+	x_ = x;
+	y_ = y;
+}
+Position operator-(const Position& pos1, const Position& pos2)
+{	return Position((pos1.x_ + pos2.x_) / 2, (pos1.y_ + pos2.y_) / 2);	}
+void Position::Display()
+{
+	cout << "두 지점의 중간 지점의 좌표는 x좌표가 " << this->x_
+		<< "이고, y좌표가 " << this->y_ << "입니다." << endl;
+}*/
+
+// 프렌드
+
