ea643728159c0a91c901cd394e132bed69b7b6f1
kate1301@naver.com <kate1301@naver.com>
2021-04-05 21:38:13 +0900

    백준) 1. 입출력과 사칙연산

---
+++ cppProject1/문제풀이.cpp
@@ -0,0 +1,58 @@
+// 1008
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << fixed;      //소수점 아래로 고정
+    cout.precision(10); //(정수포함) 전체 10자리 표현
+    cout << (double)a / b << endl;
+    return 0;
+}*/
+
+// 10869
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    // A + B, A - B, A* B, A / B(몫), A% B(나머지)
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+    return 0;
+}*/
+
+// 10430
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    // 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C,
+    // 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C
+    cout << (a + b) % c << endl;
+    cout << ((a % c) + (b % c)) % c << endl;
+    cout << (a * b) % c << endl;
+    cout << ((a % c) * (b % c)) % c << endl;
+    return 0;
+}*/
+
+// 2588
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a * (b % 10) << endl;
+    cout << a * ((b % 100) / 10) << endl;
+    cout << a * (b / 100) << endl;
+    cout << a * b << endl;
+    return 0;
+}
