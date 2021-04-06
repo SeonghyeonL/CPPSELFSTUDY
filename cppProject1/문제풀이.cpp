9803c595af0aa5bb8834ae0cd5bccb2ac4b66a64
kate1301@naver.com <kate1301@naver.com>
2021-04-06 23:54:46 +0900

    if문-ing

--- cppProject1/문제풀이.cpp
+++ cppProject1/문제풀이.cpp
@@ -1,3 +1,4 @@
+// 입출력과 사칙연산
 // 1008
 /*#include <iostream>
 using namespace std;
@@ -44,7 +45,7 @@ int main()
 }*/
 
 // 2588
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main()
 {
@@ -54,5 +55,42 @@ int main()
     cout << a * ((b % 100) / 10) << endl;
     cout << a * (b / 100) << endl;
     cout << a * b << endl;
+    return 0;
+}*/
+
+// if문
+// 1330
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a > b)
+        cout << ">" << endl;
+    else if (a < b)
+        cout << "<" << endl;
+    else if (a == b)
+        cout << "==" << endl;
+    return 0;
+}*/
+
+// 9498
+#include <iostream>
+using namespace std;
+int main()
+{
+    int grad;
+    cin >> grad;
+    if (90 <= grad && grad <= 100)
+        cout << "A" << endl;
+    else if (80 <= grad && grad <= 89)
+        cout << "B" << endl;
+    else if (70 <= grad && grad <= 79)
+        cout << "C" << endl;
+    else if (60 <= grad && grad <= 69)
+        cout << "D" << endl;
+    else if (0 <= grad && grad <= 59)
+        cout << "E" << endl;
     return 0;
 }
