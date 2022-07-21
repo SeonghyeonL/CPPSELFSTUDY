b7af4e1622b2f304b6e6873ec32ec2ead8eae315
kate1301@naver.com <kate1301@naver.com>
2022-07-21 14:04:17 +0900

    0721

---
+++ cppProject1/baekjoon.cpp
@@ -0,0 +1,39 @@
+
+// 10926
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a[51];
+    cin >> a;
+    cout << a << "??!" << endl;
+    return 0;
+}
+*/
+
+
+// 18108
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a - (2541-1998) << endl;
+    return 0;
+}
+*/
+
+
+// 25083
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "r" << endl;
+    return 0;
+}
