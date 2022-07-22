9857721f468695c2e36959ab07631be02fe58055
kate1301@naver.com <kate1301@naver.com>
2022-07-22 19:38:00 +0900

    0722

--- cppProject1/baekjoon.cpp
+++ cppProject1/baekjoon.cpp
@@ -30,10 +30,123 @@ int main()
 
 
 // 25083
+
+/*
 #include <iostream>
 using namespace std;
 int main()
 {
-    cout << "r" << endl;
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+    return 0;
+}
+*/
+
+// 2525
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int hour, min, need;
+    cin >> hour >> min >> need;
+    min += need;
+    if (min > 59)
+    {
+        hour += min / 60;
+        min = min % 60;
+    }
+    if (hour > 23)
+        hour = hour % 24;
+    cout << hour << " " << min << endl;
+    return 0;
+}
+*/
+
+// 2480
+
+/*
+#include <iostream>
+#include <algorithm> // max
+using namespace std;
+int main()
+{
+    // 같은 눈 3개 = 10000 + 같은 눈 * 1000
+    // 같은 눈 2개 = 1000 + 같은 눈 * 100
+    // 모두 다르면 = 가장 큰 눈 * 100
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a == b && b == c)
+        cout << 10000 + a * 1000;
+    else if (a == b || b == c)
+        cout << 1000 + b * 100;
+    else if (a == c)
+        cout << 1000 + a * 100;
+    else // a!=b, b!=c, c!=a
+        cout << max({ a, b, c }) * 100;
+    return 0;
+}
+*/
+
+// 17478
+
+/*
+#include <iostream>
+using namespace std;
+int pnt(int rep)
+{
+    for (int i = 0;i < rep;i++)
+        cout << "____";
+    return 0;
+}
+int jh(int n, int cnt)
+{
+    if (n > cnt)
+    {
+        pnt(cnt);
+        cout << "\"재귀함수가 뭔가요?\"" << endl;
+        pnt(cnt);
+        cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << endl;
+        pnt(cnt);
+        cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << endl;
+        pnt(cnt);
+        cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << endl;
+        jh(n, cnt + 1);
+        pnt(cnt);
+        cout << "라고 답변하였지." << endl;
+    }
+    else // n == cnt
+    {
+        pnt(cnt);
+        cout << "\"재귀함수가 뭔가요?\"" << endl;
+        pnt(cnt);
+        cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << endl;
+        pnt(cnt);
+        cout << "라고 답변하였지." << endl;
+    }
+    return 0;
+}
+int main()
+{
+    int N;
+    cin >> N;
+    cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << endl;
+    jh(N, 0);
+    return 0;
+}
+*/
+
+// 2108
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
     return 0;
 }
