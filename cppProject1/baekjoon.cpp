4e4027af23fd1c5e9c97ec1e72dc1ff290fd4578
kate1301@naver.com <kate1301@naver.com>
2022-08-07 23:04:33 +0900

    0807

--- cppProject1/baekjoon.cpp
+++ cppProject1/baekjoon.cpp
@@ -937,15 +937,44 @@ int main()
 
 // 11660
 
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int sum[1025][1025];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    int N, M, temp, x1, y1, x2, y2;
+    cin >> N >> M;
+    for (int i = 1;i <= N;i++)
+    {
+        for (int j = 1;j <= N;j++)
+        {
+            cin >> temp;
+            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + temp;
+        }
+    }
+    for (int i = 0;i < M;i++)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
+        cout << "\n";
+    }
+    return 0;
+}
+*/
+
+// 10828
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cout.tie(NULL);
-    cin.tie(NULL);
-
     return 0;
 }
