39a82860997cd3829dfe0406c872db4f2f8333a2
kate1301@naver.com <kate1301@naver.com>
2022-09-20 23:56:35 +0900

    0920

--- cppProject1/baekjoon.cpp
+++ cppProject1/baekjoon.cpp
@@ -2385,7 +2385,40 @@ int main()
 
 // 11279
 
+/*
 #include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, x;
+    priority_queue<int> heap; // 내림차순 정렬
+    cin >> N;
+    for (int i = 0;i < N;i++)
+    {
+        cin >> x;
+        if (x == 0)
+        {
+            if (heap.empty()) cout << 0 << "\n";
+            else
+            {
+                cout << heap.top() << "\n";
+                heap.pop();
+            }
+        }
+        else heap.push(x);
+    }
+    return 0;
+}
+*/
+
+//
+
+#include <iostream>
+#include <queue>
 using namespace std;
 int main()
 {
