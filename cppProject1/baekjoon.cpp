48ff71ad6453779917014215011de945becea1b0
kate1301@naver.com <kate1301@naver.com>
2022-09-22 14:29:44 +0900

    0922

--- cppProject1/baekjoon.cpp
+++ cppProject1/baekjoon.cpp
@@ -2564,6 +2564,40 @@ int main()
 
 // 11049
 
+/*
+#include <iostream>
+using namespace std;
+#define INF 1000000000
+int N;
+int m[502][2];
+int dp[502][502];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1;i <= N;i++) cin >> m[i][0] >> m[i][1];
+    for (int i = 1;i <= N - 1;i++)
+    {
+        for (int j = 1;j <= N - i;j++)
+        {
+            dp[j][i + j] = INF;
+            for (int k = j;k < i + j;k++)
+            {
+                dp[j][i + j] = min(dp[j][i + j],
+                    dp[j][k] + dp[k + 1][i + j] + m[j][0] * m[k][1] * m[i + j][1]);
+            }
+        }
+    }
+    cout << dp[1][N];
+    return 0;
+}
+*/
+
+// 1520
+
 #include <iostream>
 using namespace std;
 int main()
